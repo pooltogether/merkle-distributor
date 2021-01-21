0182da0b68f696ccb87fc788036e882d7c385033
Aodhgan <aodhgan@pooltogether.com>
2021-01-20 16:40:27 -0800

    json commas and parsing

---
+++ scripts/pre-process-json.ts
@@ -0,0 +1,18 @@
+// bigquery json (to google drive) does not have entries as an array, may still need manual love
+import { program } from 'commander'
+import fs from 'fs'
+
+program
+  .version('0.0.0')
+  .requiredOption(
+    '-i, --input <path>',
+    'input JSON file location containing a map of account addresses to string balances'
+  )
+
+program.parse(process.argv)
+
+console.log("[")
+fs.readFileSync(program.input, { encoding: 'utf8' }).split(/\r?\n/).forEach(function(line){
+   console.log(line + ",") 
+})
+console.log("]")
