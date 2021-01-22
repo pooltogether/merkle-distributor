0f0c3710d52aee65e6a4f7a23d556ee64ff303f9
Aodhgan <aodhgan@pooltogether.com>
2021-01-22 14:52:10 -0800

    added script cmds, slightly different deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,36 +1,37 @@
 import BalanceTree from '../src/balance-tree'
 const chalk = require('chalk')
-const rinkebyDistribution = require('../rinkeby.distribution.json')
+const distribution = require('../merkle_root.json')
 
 // @ts-ignore
 module.exports = async (hardhat) => {
+  console.log("Deploying MerkleDistributor")
   const { getNamedAccounts, deployments, ethers } = hardhat
   const { deploy } = deployments
   const { deployer, pool } = await getNamedAccounts()
 
-  const sortedAddresses = Object.keys(rinkebyDistribution).sort()
+   const sortedAddresses = Object.keys(distribution.claims).sort()
   const tree = new BalanceTree(
-    sortedAddresses.map((address) => ({ account: address, amount: rinkebyDistribution[address] }))
+    sortedAddresses.map((address) => ({ account: address, amount: distribution.claims[address].amount }))
   )
 
   // @ts-ignore
-  const claims = sortedAddresses.reduce((map, address, index) => {
-    // @ts-ignore
-    map[address] = {
-      index,
-      amount: rinkebyDistribution[address],
-      // @ts-ignore
-      proof: tree.getProof(index, address, rinkebyDistribution[address]),
-    }
-    return map
-  }, {})
+  // const claims = sortedAddresses.reduce((map, address, index) => {
+  //   // @ts-ignore
+  //   map[address] = {
+  //     index,
+  //     amount: distribution[address],
+  //     // @ts-ignore
+  //     proof: tree.getProof(index, address, distribution[address]),
+  //   }
+  //   return map
+  // }, {})
 
-  console.log(JSON.stringify(claims))
+  // console.log(JSON.stringify(claims))
 
-  const distributorResult = await deploy('ClearableMerkleDistributor', {
+  const distributorResult = await deploy('MerkleDistributor', {
     args: [pool, tree.getHexRoot()],
     from: deployer,
-    skipIfAlreadyDeployed: true,
+    skipIfAlreadyDeployed: false,
   })
 
   console.log(chalk.green(`Deployed MerkleDistributor: ${distributorResult.address}`))
